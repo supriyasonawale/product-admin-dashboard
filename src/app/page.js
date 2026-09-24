3b1ad5852fed9663a10b43ebc21341986bd098b3
unknown <supriyasonawale990@gmail.com>
2026-09-24 17:26:09 +0530

    Redirect home page to login

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
-export default page;
+"use client";
+
+import { useEffect } from "react";
+
+const Page = () => {
+useEffect(() => {
+window.location.href = "/login";
+}, []);
+
+return ( <div className="min-h-screen bg-slate-100 flex items-center justify-center"> <p className="text-slate-600">Redirecting to login...</p> </div>
+);
+};
+
+export default Page;
